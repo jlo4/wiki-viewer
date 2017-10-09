ffcf7379a040ebf75f8bd46add7f957717e12cbf
John Love <john.love4@gmail.com>
2017-10-09 10:23:48 +0300

    search on enter, val not empty

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,33 +4,33 @@ function getter(res) {
   $('.title').remove();
   $('.desc').remove();
   $('.link').remove();
-  var Getter = {
+  var GetterObj = {
     titles: [],
     desc: [],
     links: []
   }
   for (var i = 0; i < res.data[1].length; i++) {
-        Getter.titles.push(res.data[1][i]);
+        GetterObj.titles.push(res.data[1][i]);
     }
   for (var i = 0; i < res.data[2].length; i++) {
-        Getter.desc.push(res.data[2][i]);
+        GetterObj.desc.push(res.data[2][i]);
     }
   for (var i = 0; i < res.data[3].length; i++) {
-        Getter.links.push(res.data[3][i]);
+        GetterObj.links.push(res.data[3][i]);
     }
-  Getter.titles.forEach(function(el) {
+  GetterObj.titles.forEach(function(el) {
       $('#titlesHere').append('<li class="title">' + el + '</li>');
     })
-  Getter.desc.forEach(function(el) {
+  GetterObj.desc.forEach(function(el) {
       $('#descHere').append('<li class="desc">' + el + '</li>');
     })
-  Getter.links.forEach(function(el) {
+  GetterObj.links.forEach(function(el) {
       $('#text').append('<a class="link">' + el + '</a>');
     })
 }
 
 $(document).ready(function() {
-    $('#get').on('click', function() {
+  $('#get').on('click', function() {
     var $in = $('#search').val();
     var url = 'https://en.wikipedia.org//w/api.php?origin=*&action=opensearch&search='+ $in + '&formatversion=latest';
     axios.get(url)
@@ -42,6 +42,21 @@ $(document).ready(function() {
     })
     $('#search').val('');
   })
+  $('#search').on('keyup', function(e) {
+    var $in = $('#search').val();
+    if ($in != '' && e.which == 13) {
+      var url = 'https://en.wikipedia.org//w/api.php?origin=*&action=opensearch&search='+ $in + '&formatversion=latest';
+      axios.get(url)
+      .then(function(res){
+        getter(res);
+      })
+      .catch(function(){
+        alert(url);
+      })
+      $('#search').val('');
+    }
+  })
+
     $('#random').on('click', function() {
     var $in = $('#search').val();
     var url = 'https://en.wikipedia.org//w/api.php?origin=*&action=opensearch&search='+ $in + '&formatversion=latest';

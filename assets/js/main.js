a22a8319ec755a4df9e05bdc22c4cc9a25aabe33
John Love <john.love4@gmail.com>
2017-10-09 15:16:27 +0300

    list items, forms

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
-
-
 function getter(res) {
-  $('.title').remove();
-  $('.desc').remove();
-  $('.link').remove();
+  $('#allInfoHere').empty();
   var GetterObj = {
     titles: [],
     desc: [],
@@ -18,21 +14,23 @@ function getter(res) {
   for (var i = 0; i < res.data[3].length; i++) {
         GetterObj.links.push(res.data[3][i]);
     }
-  GetterObj.titles.forEach(function(el) {
-      $('#titlesHere').append('<li class="title">' + el + '</li>');
+  GetterObj.titles.forEach(function(el, i) {
+      $('#allInfoHere').append(`<ul class=list${i}></ul>`);
+      $(`.list${i}`).append(`<li class=title${i}>` + el + '</li>');
     })
-  GetterObj.desc.forEach(function(el) {
-      $('#descHere').append('<li class="desc">' + el + '</li>');
+  GetterObj.desc.forEach(function(el, i) {
+      $(`.list${i}`).append(`<li class=desc${i}>` + el + '</li>');
     })
-  GetterObj.links.forEach(function(el) {
-      $('#text').append('<a class="link">' + el + '</a>');
+  GetterObj.links.forEach(function(el, i) {
+      $(`.list${i}`).append(`<a class=link${i}>` + 'Link' + '</a>');
+      $(`.link${i}`).attr('href', el);
     })
 }
 
 $(document).ready(function() {
   $('#get').on('click', function() {
     var $in = $('#search').val();
-    var url = 'https://en.wikipedia.org//w/api.php?origin=*&action=opensearch&search='+ $in + '&formatversion=latest';
+    var url = 'https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search='+ $in + '&formatversion=latest';
     axios.get(url)
     .then(function(res){
       getter(res);
@@ -45,7 +43,7 @@ $(document).ready(function() {
   $('#search').on('keyup', function(e) {
     var $in = $('#search').val();
     if ($in != '' && e.which == 13) {
-      var url = 'https://en.wikipedia.org//w/api.php?origin=*&action=opensearch&search='+ $in + '&formatversion=latest';
+      var url = 'https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search='+ $in + '&formatversion=latest';
       axios.get(url)
       .then(function(res){
         getter(res);
@@ -55,19 +53,5 @@ $(document).ready(function() {
       })
       $('#search').val('');
     }
-  })
-
-    $('#random').on('click', function() {
-    var $in = $('#search').val();
-    var url = 'https://en.wikipedia.org//w/api.php?origin=*&action=opensearch&search='+ $in + '&formatversion=latest';
-    axios.get(url)
-    .then(function(res){
-      for (var i = 0; i < res.data[3].length; i++) {
-        $('#text').html(res.data[3][i] + '\n');
-      }
-    })
-    .catch(function(){
-      alert(url);
-    })
   })
 });
